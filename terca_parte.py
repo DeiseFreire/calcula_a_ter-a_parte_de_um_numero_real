9210e95382a425b0e202ede394e5ed7d9853ddf4
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2024-08-24 13:18:28 -0300

    :books: Create file

---
+++ terca_parte.py
@@ -0,0 +1,10 @@
+def calcular_terca_parte():
+  """
+  Calcula e imprime a terça parte de um número real.
+  """
+  numero = float(input("Digite um número real: "))
+  terca_parte = numero / 3
+  print(f"A terça parte de {numero} é {terca_parte}")
+
+if __name__=="__main__":
+  calcular_terca_parte()
